94fd8b6698a3cae3fbbc44206c8e19cd357d3b08
Korkmatik <fahri.korkmaz@protonmail.ch>
2019-03-31 13:59:56 +0200

    Finished the UI and basics of the game

--- game.py
+++ game.py
@@ -2,23 +2,25 @@
 
 from snake import Snake
 from food import Food
+import sys
+import pygame
+import random
 
 class Game:
 
     def __init__(self, board_size):
         self.__board_size = board_size
-        self.__snake = Snake([board_size//2, board_size//2])
-          self.__food = Food(5, 5, 50, board_size)
+        self.__food = Food(random.randint(0, board_size//4), random.randint(0, board_size//6), 50, board_size)
+        self.__snake_vel = [0, 0]
 
         self.__init_game()
 
     def start(self, FPS):
         clock = pygame.time.Clock()
 
-        self.__init()
-
-        while not game_over:
+        while not self.__game_over:
            self.__logic()
+           self.__input()
            self.__render()
 
            clock.tick(FPS)
@@ -26,14 +28,58 @@ class Game:
     def __init_game(self):
         pygame.init()
 
-        self.__screen = pygame.display.set_mode((self.board_size+150, self.board_size+100))
+        self.__BACKGROUND_COLOR = (0, 0, 0)
+        self.__score_font = pygame.font.SysFont("monospace", 35)
+        
+        self.__WIDTH = self.__board_size+150
+        self.__HEIGHT = self.__board_size+100
+ 
+        self.__snake = Snake([self.__board_size//2, self.__board_size//2], (self.__WIDTH, self.__HEIGHT))
+
+        self.__screen = pygame.display.set_mode((self.__WIDTH, self.__HEIGHT))
         self.__game_over = False
 
+    def __logic(self):
+        if self.__food.is_colliding(self.__snake.get_position(), self.__snake.get_rect_size()):
+            self.__snake.add_point()
+            self.__food.spawn()
+
+    def __input(self):
+        for event in pygame.event.get():
+            # the X button is pressed
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.__snake_vel = [-1, 0]
+                elif event.key == pygame.K_RIGHT:
+                    self.__snake_vel = [1, 0]
+                elif event.key == pygame.K_UP:
+                    self.__snake_vel = [0, -1]
+                elif event.key == pygame.K_DOWN:
+                    self.__snake_vel = [0, 1]
+
+        self.__game_over = self.__snake.move(self.__snake_vel)
+
     def __render(self):
-        self.__food.draw(screen)
-        self._snake.draw(screen)
+        self.__screen.fill(self.__BACKGROUND_COLOR)
+
+        self.__food.draw(self.__screen)
+        self.__snake.draw(self.__screen)
+
+        self.__print_score()
 
         pygame.display.update()
 
+    def __print_score(self):
+        text = "Score: " + str(self.__snake.get_score())
+        COLOR = (255, 255, 0) # yellow
+
+        label = self.__score_font.render(text, 1, COLOR)
+        self.__screen.blit(label, (5, self.__HEIGHT-40))
+
 if __name__ == '__main__':
-    pass
+    g = Game(800)
+
+    g.start(30)
--- snake.py
+++ snake.py
@@ -4,42 +4,70 @@ import pygame
 
 class Snake:
     
-    def __init__(self, start_pos):
+    def __init__(self, start_pos, board_dim):
         self.__score = 0
         self.__snake = []
         self.__snake.append(start_pos)
-        self.__symbol = symbol
-        self.__rect_size = 10
+        self.__rect_size = 50
         self.__color = (0, 255, 0)
+        self.__SPEED = 10
+        self.__board_dim = board_dim
+        self.__changed = False
 
     def move(self, amount):
         # getting position of the head
         x_pos_prev = self.__snake[0][0]
         y_pos_prev = self.__snake[0][1]
+        
+        # making it harder
+        if self.__score != 0 and not self.__changed and self.__score % 5 == 0:
+            self.__SPEED += 3 
+            self.__changed = True
+        elif self.__score % 5 != 0:
+            self.__changed = False
 
         x_pos_cur = 0
         y_pos_cur = 0
 
         # moving the head
-        self.__snake[0][0] += amount[0]
-        self.__snake[0][1] += amount[1]
+        self.__snake[0][0] += amount[0] * self.__SPEED
+        self.__snake[0][1] += amount[1] * self.__SPEED
         
+        if self.__is_colliding(x_pos_cur, y_pos_cur):
+            return True
+
         # moving the tail
         for i in range(1, len(self.__snake)):
             x_pos_cur = self.__snake[i][0]
             y_pos_cur = self.__snake[i][1]
+            
 
             # setting the current tail to the position of the tail which is ahead of them
             self.__snake[i][0] = x_pos_prev
-            self.__snake[i][1] = x_pos_prev
+            self.__snake[i][1] = y_pos_prev
             
             x_pos_prev, y_pos_prev = x_pos_cur, y_pos_cur
+
+        return False
+
+    def __is_colliding(self, x_pos, y_pos):
+        if (x_pos <= self.__snake[0][0] and x_pos >= self.__snake[0][0]+self.__rect_size) or (self.__snake[0][0] <= x_pos and self.__snake[0][0] >= x_pos+self.__rect_size):
+            if (y_pos <= self.__snake[0][1] and y_pos >= self.__snake[0][1]+self.__rect_size) or (self.__snake[0][1] <= y_pos and self.__snake[0][1] >= y_pos+self.__rect_size):
+                print("is colliding")
+                return True
+
+        pos = self.get_position()
+        
+        if pos[0] < 0 or pos[0] > self.__board_dim[0] or pos[1] < 0 or pos[1] > self.__board_dim[1]:
+            return True
+
+        return False
 
     def draw(self, screen):
         for i in range(len(self.__snake)):
             pygame.draw.rect(screen, self.__color, (self.__snake[i][0], self.__snake[i][1], self.__rect_size, self.__rect_size))
 
-    def get_postion(self):
+    def get_position(self):
         return self.__snake[0]
 
     def get_score(self):
@@ -50,7 +78,7 @@ class Snake:
 
     def add_point(self):
         back_tail = self.__snake[self.__score]
-        x_pos = back_tail[0]+1
+        x_pos = back_tail[0]
         y_pos = back_tail[1]
 
         self.__score += 1
